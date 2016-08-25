79ae877c3d7f477ca4355ad82d05299993c05649
spurdo <ananim359@gmail.com>
2016-08-25 16:00:41 +0700

    added circled lines

--- app/src/main/java/com/spudesc/cfttest/Fragments/ResponseFragment.java
+++ app/src/main/java/com/spudesc/cfttest/Fragments/ResponseFragment.java
@@ -1,14 +1,13 @@
 package com.spudesc.cfttest.Fragments;
 
 import android.app.Fragment;
+import android.graphics.CornerPathEffect;
 import android.graphics.DashPathEffect;
 import android.graphics.Paint;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.LineGraphSeries;
@@ -59,26 +58,22 @@ public class ResponseFragment extends Fragment {
         gv.setAdapter(adapter);
     }
 
-
-    /**
-     * Sets custom paint for graph, can be used to change lines to circle form.
-     */
-    private void addCustiomPaint(LineGraphSeries series) {
-        Paint paint = new Paint();
+    private void setCustomPaint(LineGraphSeries series) {
+          Paint paint = new Paint();
         paint.setStyle(Paint.Style.STROKE);
-        paint.setColor(getResources().getColor(android.R.color.white));
-        paint.setStrokeWidth(10);
-        paint.setPathEffect(new DashPathEffect(new float[]{8, 5}, 0));
+        paint.setColor(getResources().getColor(android.R.color.holo_red_light));
+        paint.setStrokeWidth(5);
+        paint.setPathEffect(new CornerPathEffect(1500f));
         series.setCustomPaint(paint);
     }
 
     private void prepareGraphView(GraphView graphView) {
         LineGraphSeries series = new LineGraphSeries(graphPoints);
-        addCustiomPaint(series);
-
+        setCustomPaint(series);
+        graphView.removeAllSeries();
         graphView.addSeries(series);
         graphView.computeScroll();
-        graphView.getViewport().setScalable(true);
+        graphView.getViewport().setScalable(true); //works, but conflicts with scrollview
 
     }
 }
