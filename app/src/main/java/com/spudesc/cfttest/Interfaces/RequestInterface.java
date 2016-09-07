966d2560a174d0b6a316471e369e273cc762ad31
spurdo <ananim359@gmail.com>
2016-09-07 13:27:51 +0700

    deleted for rename

--- app/src/main/java/com/spudesc/cfttest/Interfaces/RequestInterface.java
+++
@@ -1,8 +0,0 @@
-package com.spudesc.cfttest.interfaces;
-
-/**
- * Created by Roman Babenko (rbab@yandex.ru) on 23.08.2016.
- */
-public interface RequestInterface {
-    void requestPoints(int count);
-}
